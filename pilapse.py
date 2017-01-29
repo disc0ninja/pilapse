9c1d850f8aecf70aae6593954e2ddfaa9be2c5ec
disc0ninja <disc0ninja77@gmail.com>
2017-01-29 07:07:28 +0000

    imported time to pilapse script. began slicing and formatting yime for use in camera control function.

--- pilapse.py
+++ pilapse.py
@@ -1,9 +1,17 @@
 #pilapse
 
+import time
 import picamera
+from time import strftime, gmtime
 
+tyme = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 camera = picamera.PiCamera()
 
 camera.capture('image.jpg')
 
 print "your picture is done"
+print "the current time is " + str(tyme)
+
+piTime = tyme[16:22]
+
+print piTime
